63c0edc2c36d9e1022fe07ef1d9a46e635993fab
thanadonice57 <107331365+thanadonice57@users.noreply.github.com>
2022-06-12 15:52:05 +0700

    person.py

---
+++ person.py
@@ -0,0 +1,28 @@
+class Person:
+    def __init__(self,name,gender,profession) -> None:
+        self.name = name
+        self.gender = gender
+        self.profession = profession
+        self.study = 0
+        
+   
+    def work(self):
+        print(f'{self.name} working as a {self.profession}')
+    
+    def study(self,hours):
+        self.study = hours
+   
+    def show(self):
+        print(f'Name:{self.name} Gender:{self.gender} Profession:{self.profession} Study:{self.study}')
+        
+
+#person1
+jessa = Person('Jessa','Female','Software Engineer')
+jessa.work()
+jessa.show()
+
+#person2
+jon = Person('Jon','male','Doctor')
+jon.work()
+jon.study = 15
+jon.show()
