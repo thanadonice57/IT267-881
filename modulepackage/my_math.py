953af59fbcf7c0a6684f425af15a96eaad01ebed
thanadonice57 <107331365+thanadonice57@users.noreply.github.com>
2022-06-26 09:50:40 +0700

    basicoop

---
+++ modulepackage/my_math.py
@@ -0,0 +1,9 @@
+# my_math.py
+PI = 3.1416
+
+def sqrt(number):
+    return (number * number)
+
+def circle_area(radius):
+    area = PI * (radius ** 2)
+    return area
---
+++ modulepackage/my_math_use.py
@@ -0,0 +1,5 @@
+#วิธีที่1 
+from my_math import sqrt,circle_area
+
+print(f'sqrt of 5 = {sqrt(5)}')
+print(f'circle area = {circle_area(2):,.2f}')
